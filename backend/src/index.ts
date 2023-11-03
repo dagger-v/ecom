debbf56616653248e49cfad31571efd0c972c4b1
dagger-v <goingarcher@gmail.com>
2023-11-03 02:44:06 -0700

    added mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,23 @@
 import express from "express";
-import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: __dirname + "../../../.env" });
+import cors from "cors";
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect();
+// connect to DB
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGO_URI;
 
-app.listen(5000, () => console.log("Server started"));
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
